38acbced3672e1f9cd73bb94b295560e191ca632
Thiago Hernandes Souza <thernans@everis.com>
2020-03-17 13:17:41 -0300

    Criação do projeto

---
+++ crud-mariadb/src/main/java/com/crud/api/CrudMariadbApplication.java
@@ -0,0 +1,13 @@
+package com.crud.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudMariadbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudMariadbApplication.class, args);
+	}
+
+}
